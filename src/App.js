5320a6a586a2568a6b72c3b0ba1ad4fe1bdfb7ea
mchostak <mchostak@ztrax.com>
2022-06-02 10:34:24 -0300

    create a new circular percentage bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import './script'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div className='circular-progress'>
+            <div className='value-container'>0%</div>
+          </div>
+        </div>
       </header>
     </div>
   );
